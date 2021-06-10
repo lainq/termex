542203b5308fe4f44cdc9c219cabeaf1e31d3228
fineans <82938580+dopevog@users.noreply.github.com>
2021-06-10 10:58:08 +0530

    update list.py

--- list.py
+++ list.py
@@ -1,9 +1,12 @@
 import os
 import time
+import getpass
 
 from rich.console import Console
 from rich.syntax import Syntax
 from rich.table import Table
+from rich.text import Text
+
 
 
 class ListDirectories(object):
@@ -18,7 +21,10 @@ class ListDirectories(object):
 
     def list_directory_content(self):
         files = os.listdir(self.path.filename)
-        table = Table(title=self.path.filename)
+        directory = self.path.filename
+        directory = directory[1:]
+        directory = Text(directory,style="bold")
+        table = Table(title=directory)
 
         table.add_column("Name", justify="center", style="cyan", no_wrap=True)
         table.add_column("Type", style="magenta")
@@ -36,7 +42,14 @@ class ListDirectories(object):
             table.add_row(
                 filename, file_type, extension, f"{size} bytes", modified_time
             )
+        
+
+        console = Console()
         self.console.print(table)
+        fileno = Text("| File 1 of 1",style="bold blue")
+        user = Text(getpass.getuser().rjust(28, ' '),style="bold blue")
+        helpinstructions = Text("h=Help |".rjust(38, ' '),style="bold blue")
+        console.print(fileno+user+helpinstructions)
 
     def open_file(self):
         """Show the content of a file in the terminal"""
