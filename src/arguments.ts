6f6f58ecb93164f2a3c7d664895074c5b76a4b19
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-06-13 21:44:59 +0530

    fmt:Formatted the code

--- src/arguments.ts
+++ src/arguments.ts
@@ -1,32 +1,53 @@
-import { argv } from "process";
-import { CommandLineException } from "./exception";
+import {argv} from 'process';
+import {CommandLineException} from './exception';
 
 export interface ArgumentParserResults {
-    command:string,
-    parameters:Array<string>
+  // The command that the user wants to run
+  // Eg := tree, run, list, help etc
+  // if the length of the arguments is 0
+  // The details about the current working directory
+  // is displayed
+  command: string;
+  // The flags passed in along with the command
+  // Eg := --only-dirs, --gitignore
+  // Flags should start with double hiphens(--)
+  parameters: Array<string>;
 }
 
 export class ArgumentParser {
-    private readonly arguments:Array<string> = argv.slice(2)
+  // The list of command line arguments
+  private readonly arguments: Array<string> = argv.slice(2);
 
-    public parseArguments():ArgumentParserResults {
-        let command:string = 'help'
-        let parameters:Array<string> = new Array<string>()
-        for(let index=0; index<this.arguments.length; index++){
-            const currentArgument:string = this.arguments[index]
-            if (index == 0){
-                command = currentArgument
-                continue
-            }
-            const isValidArgument:boolean = currentArgument.startsWith("--")
-            if(!isValidArgument){
-                const error = new CommandLineException({
-                    message : `${currentArgument} is not a valid argument`,
-                    suggestion : "Parameter keys should start with --"
-                })
-            }
-            parameters.push(currentArgument)
-        }
-        return {command:command, parameters:parameters}
+  /**
+   * @oublic
+   *
+   * The argument parser function that
+   * parses the list if command line arguments
+   * into commands and parameters(or flags)
+   *
+   * @returns {ArgumentParserResults} The command and the flags
+   */
+  public parseArguments(): ArgumentParserResults {
+    let command: string = 'help';
+    let parameters: Array<string> = new Array<string>();
+    for (let index = 0; index < this.arguments.length; index++) {
+      const currentArgument: string = this.arguments[index];
+      if (index == 0) {
+        // If the length of the list is greater than 0
+        // The first element of the list is taken as the
+        // command
+        command = currentArgument;
+        continue;
+      }
+      const isValidArgument: boolean = currentArgument.startsWith('--');
+      if (!isValidArgument) {
+        const error = new CommandLineException({
+          message: `${currentArgument} is not a valid argument`,
+          suggestion: 'Parameter keys should start with --',
+        });
+      }
+      parameters.push(currentArgument);
     }
+    return {command: command, parameters: parameters};
+  }
 }
--- src/exception.ts
+++ src/exception.ts
@@ -1,27 +1,40 @@
-import { redBright, yellowBright } from "chalk"
+import {redBright, yellowBright} from 'chalk';
 
 interface ErrorMessage {
-    message: string
-    suggestion? : string
+  // The error message
+  message: string;
+  // Any suggestion that can help in solving
+  // the issue
+  suggestion?: string;
 }
 
 export class CommandLineException {
-    private readonly message:string
-    private readonly suggestion?:string
+  private readonly message: string;
+  private readonly suggestion?: string;
 
-    constructor(message:ErrorMessage, isFatal:boolean = true, exitStatusCode:number = 1){
-        this.message = message.message
-        this.suggestion = message.suggestion
-        this.throw(isFatal, exitStatusCode)
-    }
+  /**
+   * @constructor
+   * @param {ErrorMessage} message The error message and the suggestion message
+   * @param {boolean} isFatal Whether the program should exit after throwing the errpr
+   * @param {number} exitStatusCode The exit status code
+   */
+  constructor(
+    message: ErrorMessage,
+    isFatal: boolean = true,
+    exitStatusCode: number = 1
+  ) {
+    this.message = message.message;
+    this.suggestion = message.suggestion;
+    this.throw(isFatal, exitStatusCode);
+  }
 
-    private throw(isFatal:boolean, exitStatusCode:number) {
-        console.log(redBright(this.message))
-        if (this.suggestion){
-            console.log(yellowBright(this.suggestion))
-        }
-        if (isFatal){
-            process.exit(exitStatusCode)
-        }
+  private throw(isFatal: boolean, exitStatusCode: number) {
+    console.log(redBright(this.message));
+    if (this.suggestion) {
+      console.log(yellowBright(this.suggestion));
     }
+    if (isFatal) {
+      process.exit(exitStatusCode);
+    }
+  }
 }
