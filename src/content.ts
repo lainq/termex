b418f435bd469ad989dbd50dac7c693e6564fbff
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-06-22 17:20:44 +0530

    feat:Language percent

--- src/content.ts
+++ src/content.ts
@@ -1,6 +1,9 @@
+import { cyanBright, yellowBright } from "chalk";
 import { readFileSync } from "fs";
+import { isBinaryFileSync } from "isbinaryfile";
 import { lookup } from "mime-types";
 import { cwd } from "process";
+import { table } from "table";
 import { File } from "./utils";
 import { Walk } from "./walk";
 
@@ -11,13 +14,46 @@ interface Count {
 }
 export class ContentPercent {
     private readonly path:string = cwd()
+    private files:number = 0
+    private lines:number = 0
     
     constructor(parameters:Array<string>){
         const files:Array<string> = new Walk(this.path, parameters).files
         const mime = this.mimeTypes(files)
         
         const stats:Map<string, Count> = this.createStats(mime)
-        console.log(stats)
+        const percent:Map<string, Count> = this.createPercent(stats)
+
+        this.displayTable(percent)
+    }
+
+    private displayTable = (data:Map<string, Count>):void => {
+        let tableData:Array<Array<string>> = [[cyanBright("Type"), yellowBright("Files"), yellowBright("Lines")]]
+        for(const key of Array.from(data.keys())) {
+            const element:Count | undefined = data.get(key)
+            if(!element) { continue }
+            tableData.push([
+                element.mimeType,
+                `${element.files}%`,
+                `${element.lines}%`
+            ])
+        }
+        console.log(table(tableData))
+    }
+
+    private createPercent = (stats:Map<string, Count>):Map<string, Count>=> {
+        let percent:Map<string, Count> = stats
+        let keys:Array<string> = Array.from(percent.keys())
+        for(let index=0; index<keys.length; index++){
+            const element:Count | undefined = percent.get(keys[index])
+            if(!element) { continue }
+            percent.set(keys[index], {
+                mimeType : element.mimeType,
+                files: parseFloat(((element.files / this.files) * 100).toFixed(1)),
+                lines: parseFloat(((element.lines / this.lines) * 100).toFixed(1))
+            })
+        }
+        return percent
     }
 
     private mimeTypes = (files:Array<string>):Map<string, Array<string>> => {
@@ -25,6 +61,10 @@ export class ContentPercent {
         for(let index=0; index<files.length; index++){
             const file:string = files[index]
             const type:string | false = lookup(file)
+
+            if(isBinaryFileSync(file)){
+                continue
+            }
 
             const typeString:string = type == false ? "unknown" : type.split("/").slice(1)[0]
             let match:Array<string> | undefined = typeMap.get(typeString)
@@ -47,6 +87,7 @@ export class ContentPercent {
             if(!element){ continue }
             for(let fileIndex=0; fileIndex<element.length; fileIndex++){
                 const currentFile:string = element[fileIndex]
+                this.files += 1
                 linesOfCode += readFileSync(currentFile).toString().split('\n').length
             }
             const count:Count = {
@@ -55,7 +96,9 @@ export class ContentPercent {
                 lines: linesOfCode
             }
             data.set(keys[index], count)
+            this.lines += linesOfCode
         }
+
         return data
     }
 }
