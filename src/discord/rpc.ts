22639e4f8a701c7440c4be1bf5e65008c82ee809
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-06-24 23:00:10 +0530

    feat:RPC enabling

--- src/discord/rpc.ts
+++ src/discord/rpc.ts
@@ -1,8 +1,11 @@
-import { redBright, yellowBright } from "chalk";
+import { cyanBright, redBright, yellowBright } from "chalk";
 import { Client } from "discord-rpc";
-import { readFileSync, writeFile } from "fs";
+import { readFileSync, writeFile, writeFileSync } from "fs";
 import { homedir } from "os";
 import { join } from "path";
+import { stdin, stdout } from "process";
+import { createInterface, Interface } from "readline";
+import { CommandLineException } from "../exception";
 import { checkFileExists } from "../utils";
 
 export class RichPresenceSettings {
@@ -21,18 +24,6 @@ export class RichPresenceSettings {
       .trim();
     return key.length == 0 ? null : key;
   };
-}
-
-export class RichPresenceSetup {
-  private parameters: Array<string>;
-
-  constructor(parameters: Array<string>) {
-    this.parameters = parameters;
-
-    const enable = this.parameters.includes("enable");
-    if (enable) {
-    }
-  }
 }
 
 export class TermexDiscordRPC {
@@ -71,6 +62,61 @@ export class TermexDiscordRPC {
 
     this.client.login({ clientId }).catch((reason: any): void => {});
   };
+}
+
+
+export class RichPresenceSetup {
+  private parameters: Array<string>;
+  private readlineInterface:Interface = createInterface({
+    input: stdin,
+    output: stdout
+  })
+
+  constructor(parameters: Array<string>) {
+    this.parameters = parameters;
+
+    const enable = this.parameters.includes("enable");
+    if (enable) {
+      this.enableRichPresence()
+    } else {
+      this.displayStatus()
+    } 
+    process.exit()
+  }
+
+  private displayStatus = ():any => {
+    const clientId:string | null = RichPresenceSettings.getClientId()
+    if(!clientId){
+      console.log(`${cyanBright("Enabled:")} ${redBright("False")}`)
+      process.exit()
+    }
+    console.log(`${cyanBright("Enabled:")} ${yellowBright("True")}`)
+  }
+
+  private enableRichPresence = ():any => {
+    const clientId:string | null = RichPresenceSettings.getClientId()
+    if(clientId != null){
+      console.log(yellowBright("RPC is already enabled"))
+      process.exit()
+    }
+    const numbers:Array<string> = Array.from(Array(10).keys()).map((value:number):string => {return value.toString()})
+    const key = this.readlineInterface.question(cyanBright("Client Id [?] "), (solution:string):void => {
+      const match:Array<string> = solution.trim().split("").filter((value:string):boolean => {
+        return numbers.includes(value)
+      })
+      if(match.length != solution.length){
+        new CommandLineException({
+          message: "Invalid client id"
+        })
+      }
+      writeFile(RichPresenceSettings.settingsFile, solution.trim(), (error:NodeJS.ErrnoException | null):any => {
+        if(error){
+          new CommandLineException({message:error.toString()})
+        }
+      })
+      this.readlineInterface.close()
+    })
+  }
 }
 
 // const key = RichPresenceSettings.getClientId()
