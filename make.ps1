fb5ec4a05c4e633448c9078f5ee83449f675abca
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-06-30 18:08:59 +0530

    Build scripts

---
+++ make.ps1
@@ -0,0 +1,10 @@
+echo "Building from source"
+tsc index.ts --esModuleInterop true --allowJs true --outDir ./dist
+echo "Installing node-gyp and pkg"
+npm install node-gyp pkg
+echo "Installing other dependencies"
+npm install
+
+echo "Building using pkg"
+pkg -t node14-win -c package.json -o ./executable/termex.exe ./dist/index.js
+[Environment]::Exit(0)
